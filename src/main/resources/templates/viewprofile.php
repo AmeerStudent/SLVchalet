3fa3715e803025c8e1c511586eb21d999090cdad
AmeerStudent <150236196+AmeerStudent@users.noreply.github.com>
2024-01-13 19:47:44 +0800

    Update viewprofile.php

--- src/main/resources/templates/viewprofile.php
+++ src/main/resources/templates/viewprofile.php
@@ -1,6 +1,7 @@
-<?php
-include 'inc/connection.php';
+<!-- <?php
+include  ("/../inc/connection.php")
 session_start();
+?> -->
 
 $id = $_GET['id'];
 
@@ -40,16 +41,16 @@ if (mysqli_num_rows($result) == 0) {
 
   <!-- Favicons -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="assets/img/ipm-logo-01.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link th:href="@{assets/img/ipm-logo-01.png}" rel="icon">
+  <link th:href="@{assets/img/apple-touch-icon.png}" rel="apple-touch-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link th:href="@{assets/vendor/bootstrap/css/bootstrap.min.css}" rel="stylesheet">
+  <link th:href="@{assets/vendor/bootstrap-icons/bootstrap-icons.css}" rel="stylesheet">
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
@@ -58,7 +59,7 @@ if (mysqli_num_rows($result) == 0) {
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link th:href="@{assets/css/style.css}" rel="stylesheet">
 
 </head>
 
@@ -166,18 +167,18 @@ if (mysqli_num_rows($result) == 0) {
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/purecounter/purecounter.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script th:src="@{assets/vendor/bootstrap/js/bootstrap.bundle.js}"></script>
+  <script th:src="@{assets/vendor/aos/aos.js}"></script>
+  <script th:src="@{assets/vendor/php-email-form/validate.js}"></script>
+  <script th:src="@{assets/vendor/swiper/swiper-bundle.min.js}"></script>
+  <script th:src="@{assets/vendor/purecounter/purecounter.js}"></script>
+  <script th:src="@{assets/vendor/isotope-layout/isotope.pkgd.min.js}"></script>
+  <script th:src="@{assets/vendor/glightbox/js/glightbox.min.js}"></script>
+  <script th:src="@{https://code.jquery.com/jquery-3.5.1.min.js}"></script>
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script th:src="@{assets/js/main.js}"></script>
 </body>
 
 </html>
