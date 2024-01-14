58c4d7c32b5cffca1055ed13440d25a1a16d562d
najwazawani <150237870+najwazawani@users.noreply.github.com>
2024-01-14 17:05:49 +0800

    Add files via upload

---
+++ src/main/java/com/heroku/java/MODEL/staff.java
@@ -0,0 +1,58 @@
+package com.heroku.java.MODEL;
+
+public class staff {
+    private String staffId;
+	private String staffName;
+	private String staffEmail;
+	private String staffPhoneNo;
+	private String staffPass;
+	private String adminId;
+
+	public String getStaffId() {
+		return staffId;
+	}
+
+	public void setStaffId(String staffId) {
+		this.staffId = staffId;
+	}
+
+	public String getStaffName() {
+		return staffName;
+	}
+
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+
+	public String getStaffEmail() {
+		return staffEmail;
+	}
+
+	public void setStaffEmail(String staffEmail) {
+		this.staffEmail = staffEmail;
+	}
+
+	public String getStaffPhoneNo() {
+		return staffPhoneNo;
+	}
+
+	public void setStaffPhoneNo(String staffPhoneNo) {
+		this.staffPhoneNo = staffPhoneNo;
+	}
+
+	public String getStaffPass() {
+		return staffPass;
+	}
+
+	public void setStaffPass(String staffPass) {
+		this.staffPass = staffPass;
+	}
+
+	public String getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+}
