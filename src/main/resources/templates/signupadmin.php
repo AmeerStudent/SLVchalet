c7cb926b73f2782fe2cb2a59ac2da3e593a1b54f
najwazawani <150237870+najwazawani@users.noreply.github.com>
2024-01-14 13:18:13 +0800

    Update signupadmin.php

--- src/main/resources/templates/signupadmin.php
+++ src/main/resources/templates/signupadmin.php
@@ -1,7 +1,7 @@
-<?php 
-  session_start();
+// <?php 
+//   session_start();
 
-?>
+// ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,15 +24,15 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link th:href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link th:href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link th:href="@{assets/css/style.css}" rel="stylesheet" type = "text/css"/> <!-- tukar thref -->
 
 </head>
 
