a28b74e11f4bd9825980b5641a117a94ab350989
najwazawani <150237870+najwazawani@users.noreply.github.com>
2024-01-15 19:54:13 +0800

    Update LoginController.java

--- src/main/java/com/heroku/java/CONTROLLER/LoginController.java
+++ src/main/java/com/heroku/java/CONTROLLER/LoginController.java
@@ -160,12 +160,14 @@ public class LoginController {
         }
         
         }
+        }
+
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/";
     }
 }
-}
+
 
     
