c85d36c44c146704fe9632822fcb4d235682ae19
najwazawani <150237870+najwazawani@users.noreply.github.com>
2024-01-15 19:26:47 +0800

    Update FacilitiesController.java

--- src/main/java/com/heroku/java/CONTROLLER/FacilitiesController.java
+++ src/main/java/com/heroku/java/CONTROLLER/FacilitiesController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.multipart.MultipartFile;
 import com.heroku.java.MODEL.*;
 import com.heroku.java.MODEL.facility;
 import com.heroku.java.MODEL.room;
