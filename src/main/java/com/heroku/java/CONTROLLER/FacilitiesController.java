f8433af09892a1b11cf7ccffed939f51afb8e1e6
najwazawani <150237870+najwazawani@users.noreply.github.com>
2024-01-24 16:18:20 +0800

    Update FacilitiesController.java

--- src/main/java/com/heroku/java/CONTROLLER/FacilitiesController.java
+++ src/main/java/com/heroku/java/CONTROLLER/FacilitiesController.java
@@ -124,7 +124,7 @@ public class FacilitiesController {
                 int facilityQtty = resultSet.getInt("facilityQtty");
                 String facilityDescription = resultSet.getString("facilityDescription");
                 String facilityType = resultSet.getString("facilityType");
-                String facilityPic = resultSet.getString("facilityPic");
+                 byte[] facilityPic = resultSet.getByte[]("facilityPic");
                 String staffId = resultSet.getString("staffId");
                 facility facility;
                 if (facilityType.equalsIgnoreCase("room")) {
