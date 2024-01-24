662a79cf7d6fa1230ab3ef80dccc55c7936492d6
najwazawani <150237870+najwazawani@users.noreply.github.com>
2024-01-24 21:39:28 +0800

    Add files via upload

---
+++ src/main/java/com/heroku/java/MODEL/reservation.java
@@ -0,0 +1,64 @@
+package com.heroku.java.MODEL;
+public class reservation {
+	private String reserveId;
+	private Date reserveCheckIn;
+	private Date reserveCheckOut;
+	private String reserveStatus;
+	private String paymentStatus;
+	private String receipt;
+
+	public reservationBean() {
+
+	}
+
+	public String getReserveId() {
+		return reserveId;
+	}
+
+	public void setReserveId(String reserveId) {
+		this.reserveId = reserveId;
+	}
+
+	
+
+	public Date getReserveCheckIn() {
+		return reserveCheckIn;
+	}
+
+	public void setReserveCheckIn(Date reserveCheckIn) {
+		this.reserveCheckIn = reserveCheckIn;
+	}
+
+	public Date getReserveCheckOut() {
+		return reserveCheckOut;
+	}
+
+	public void setReserveCheckOut(Date reserveCheckOut) {
+		this.reserveCheckOut = reserveCheckOut;
+	}
+
+	public String getReserveStatus() {
+		return reserveStatus;
+	}
+
+	public void setReserveStatus(String reserveStatus) {
+		this.reserveStatus = reserveStatus;
+	}
+
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	public String getReceipt() {
+		return receipt;
+	}
+
+	public void setReceipt(String receipt) {
+		this.receipt = receipt;
+	}
+
+}
